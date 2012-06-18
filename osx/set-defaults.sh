857477f89d284808d9813154a95bb23fdba357a0
Alex Rudy <alex.rudy@gmail.com>
2012-06-18 18:54:51 +0800

    Adding modified files for git and osx

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -15,6 +15,3 @@ defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
 
 # Always open everything in Finder's list view. This is important.
 defaults write com.apple.Finder FXPreferredViewStyle Nlsv
-
-# Show the ~/Library folder
-chflags nohidden ~/Library
