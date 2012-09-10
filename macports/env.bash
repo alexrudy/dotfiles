ec6f6b5b0e2d043d1fa9117fa438f6d2f6738a09
Alex Rudy <alex.rudy@gmail.com>
2012-09-10 14:45:37 -0700

    Fixing LIBRARY_PATH which was missing colons

--- macports/env.bash
+++ macports/env.bash
@@ -1,4 +1,4 @@
-export LD_LIBRARY_PATH=/opt/local/lib:/usr/local/lib$LD_LIBRARY_PATH
-export LIBRARY_PATH=/opt/local/lib
+export LD_LIBRARY_PATH=/opt/local/lib:/usr/local/lib:$LD_LIBRARY_PATH
+export LIBRARY_PATH=/opt/local/lib:/usr/local/lib:
 export CPATH=/opt/local/include
-export DYLD_FALLBACK_LIBRARY_PATH=$DYLD_FALLBACK_LIBRARY_PATH/opt/local/libx
+export DYLD_FALLBACK_LIBRARY_PATH=$DYLD_FALLBACK_LIBRARY_PATH:/opt/local/libx
