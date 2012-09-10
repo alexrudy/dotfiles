f0e9739de10e786e51f45c9a7af7602a849010ea
Alex Rudy <alex.rudy@gmail.com>
2012-09-10 14:52:45 -0700

    Supporting ipython profiles

---
+++ python/ipython.dir/profile_astroobject/startup/ao-modules.py
@@ -0,0 +1,15 @@
+# 
+#  ao-modules.py
+#  profile_astroobject
+#  
+#  Created by Alexander Rudy on 2012-09-07.
+#  Copyright 2012 Alexander Rudy. All rights reserved.
+# 
+import AstroObject as AO
+import AstroObject.AstroImage as AImg
+import AstroObject.AstroSimulator as ASim
+import AstroObject.AstroCache as ACac
+import AstroObject.AstroConfig as ACfg
+import AstroObject.AstroSpectra as ASpec
+import AstroObject.AnalyticSpectra as AASpec
+import AstroObject.util as AUtil
