a2953f560b3975e6daf8547304a97c3330966aa9
Alex Rudy <alex.rudy@gmail.com>
2012-10-07 10:56:14 -0700

    Macports files check for macports

--- bash/prompt.bash
+++ bash/prompt.bash
@@ -1,8 +1,13 @@
 # Custom Prompt Function
-function prompt() {
-    export SYS_PS1=$PS1
-    #export PS1="\[\033[0;36m\]\h:\[\033[01;34m\]\W\[\033[00m\] \u\$ "
-	# export PS1="\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ "
+prompt=/opt/local/share/doc/git-core/contrib/completion/git-prompt.sh
+export SYS_PS1=$PS1
+if [ -f $prompt ]; then
 	PS1='[\[\033[0;36m\]\u@\h \[\033[01;34m\]\W\[\033[00m\]$(__git_ps1 " (%s)")]\$ '
-}
-prompt
+else
+	PS1='[\[\033[0;36m\]\u@\h \[\033[01;34m\]\W\[\033[00m\]]\$ '
+fi
+
+# OLD PS1's
+# export PS1="\[\033[0;36m\]\h:\[\033[01;34m\]\W\[\033[00m\] \u\$ "
+# export PS1="\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ "
+	
