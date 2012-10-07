579817abbc9b9d9f24c362ae17ace12e2fc63d87
Alex Rudy <alex.rudy@gmail.com>
2012-10-07 11:39:13 -0700

    Removed references to static path home

--- caos/aliases.bash
+++ caos/aliases.bash
@@ -2,7 +2,7 @@
 #Astronomy Software
 #==================================
 
-CAOS_DIR="/Users/alexanderrudy/Development/Astronomy/IDL/work_caos"
+CAOS_DIR="$HOMEDevelopment/Astronomy/IDL/work_caos"
 if [ -d $CAOS_DIR ]; then
-	alias caos='cd /Users/alexanderrudy/Development/Astronomy/IDL/work_caos ; source caos_env.sh ; idl'
+	alias caos='cd $HOMEDevelopment/Astronomy/IDL/work_caos ; source caos_env.sh ; idl'
 fi
