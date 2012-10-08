81cc9bd37c623d6876995066e724f872b5ab49f5
Alex Rudy <alexrudy@hades.ucsc.edu>
2012-10-08 10:46:54 -0700

    fixing mate command

--- tm/env.bash
+++ tm/env.bash
@@ -6,6 +6,6 @@
 #  Copyright 2012 Alexander Rudy. All rights reserved.
 # 
 if command_exists mate; then
-	export EDITOR=mate
+	export EDITOR="mate -w"
 	export GIT_EDITOR="mate --name 'Git Commit Message' -w -l 1"
 fi
