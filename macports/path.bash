557f50ddbf217cd2d74225700b914cc49cb93211
Alex Rudy <alex.rudy@gmail.com>
2012-10-16 11:16:51 -0700

    Adding MPPREFIX variable

--- macports/path.bash
+++ macports/path.bash
@@ -1,3 +1,3 @@
-if [ -f /opt/local/bin/port ]; then
-	export PATH=/opt/local/bin:/opt/local/sbin:$PATH                            #Macports Path Prefix
+if [ -f "$MPPREFIX/bin/port" ]; then
+	export PATH="$MPPREFIX/bin:$MPPREFIX/sbin:$PATH"                            #Macports Path Prefix
 fi
