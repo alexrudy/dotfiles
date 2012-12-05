3bccc22441fec0a7a6e64cad6e11b7db89d09d2a
Alex Rudy <alex.rudy@gmail.com>
2012-12-04 22:08:35 -0800

    Updated install

--- install.sh
+++ install.sh
@@ -10,20 +10,25 @@
 
 files=`find */*.symlink`
 cwd=`pwd`
+update=false
 
 for file in $files
 do
 	dest=${file%%.symlink}
 	dest="."${dest##*/}
-	if [[ -e $HOME/$dest ]]; then
+	if [[ -L "$HOME/$dest" ]]; then
+		echo "Updating link to $file"
+		rm "$HOME/$dest"
+		ln -s "$cwd/$file" "$HOME/$dest"
+	elif [[ -e $HOME/$dest ]]; then
 		echo "File $dest already exists. Skipping"
 	else
 		echo "Linking $cwd/$file to $dest"
-		ln -s $cwd/$file $HOME/$dest
+		ln -s "$cwd/$file" "$HOME/$dest"
 	fi
 done
 
-directories=`find ./*/*.dir -type d -maxdepth 0`
+directories=`find */*.dir -type d -maxdepth 0`
 
 for directory in $directories
 do
@@ -31,10 +36,14 @@ do
 	dest="."${dest##*/}
 	if [ ! -d $directory ]; then
 		echo "$directory is not a directory"
+	elif [[ -L "$HOME/$dest" ]]; then
+		echo "Updating link to $directory"
+		rm "$HOME/$dest"
+		ln -s "$cwd/$directory" "$HOME/$dest"	
 	elif [ -e $HOME/$dest ]; then
 		echo "Directory $dest already exists. Skipping"
 	else
 		echo "Linking $directory to $dest"
-		ln -s $cwd/$directory $HOME/$dest
+		ln -s "$cwd/$directory" "$HOME/$dest"
 	fi
 done
