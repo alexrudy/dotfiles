cd61417a9a1b68da02e268156f71295999bbb67d
Alex Rudy <alex.rudy@gmail.com>
2013-10-25 15:08:01 -0400

    Adding the perl bin path to the macports path exports

--- macports/path.bash
+++ macports/path.bash
@@ -1,3 +1,4 @@
 if [ -f "$MPPREFIX/bin/port" ]; then
-	export PATH="$MPPREFIX/bin:$MPPREFIX/sbin:$PATH"                            #Macports Path Prefix
+    export PATH="$MPPREFIX/bin:$MPPREFIX/sbin:$PATH"                            #Macports Path Prefix
+    export PATH="$PATH:$MPPREFIX/libexec/perl5.12/sitebin"
 fi
