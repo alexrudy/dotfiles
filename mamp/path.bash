d43a059bc00b6f6d92ef606df8a5092be190eb08
Alex Rudy <alex.rudy@gmail.com>
2013-10-30 22:35:59 -0700

    Path concealing MAMP and node

--- mamp/path.bash
+++ mamp/path.bash
@@ -1,1 +1,3 @@
-export PATH=$PATH:/Applications/MAMP/bin:/Applications/MAMP/bin/apache2/bin #MAMP Tools
+if [ -d "/Applications/MAMP/bin" ]; then
+    export PATH=$PATH:/Applications/MAMP/bin:/Applications/MAMP/bin/apache2/bin #MAMP Tools
+fi
--- node/path.bash
+++ node/path.bash
@@ -1,1 +1,3 @@
-export PATH=$PATH:~/.bin/node/bin											#node.js support
+if [ -d ~/.bin/node/bin ]; then
+    export PATH=$PATH:~/.bin/node/bin #node.js support
+fi
