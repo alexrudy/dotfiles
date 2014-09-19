2b33dad38db213c1541be3f58ab7d3b065494542
Alex Rudy <alex.rudy@gmail.com>
2014-09-18 20:52:35 -0700

    Many bug fixes

--- python/path.bash
+++ python/path.bash
@@ -5,5 +5,14 @@
 # Installed via macports, so we only run it here.
 if [ -f $MPPREFIX/bin/port ]; then
     export WORKON_HOME="$HOME/.virtualenvs/"
+    if [ -d "$HOME/Development/" ]; then
+        export PROJECT_HOME="$HOME/Development/"
+    fi
     source /opt/local/bin/virtualenvwrapper.sh
 fi
+
+activatepy () {
+    in_pwd=`pwd`
+    workon "$@"
+    cd $in_pwd
+}
