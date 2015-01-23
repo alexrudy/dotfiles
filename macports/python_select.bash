e535ba1bd937c77f2008e9a0780f385c36778bb3
Alex Rudy <alex.rudy@gmail.com>
2015-01-22 17:08:28 -0800

    Python select fixes

--- macports/python_select.bash
+++ macports/python_select.bash
@@ -4,7 +4,7 @@
 #PYTHON Variables
 
 function port_python_version () {
-  if [[ -z $PYVERSION ]]; then
+  if [ -z $PYVERSION ]; then
     local GETPYTHONREGEX="s/^.*py[A-Za-z]+([0-9])([0-9]+)-?[A-Za-z]*.*$/\1.\2/"
     local COMMAND="port select --show python"
     PYVERSION=`$COMMAND  | sed -E $GETPYTHONREGEX`
@@ -30,7 +30,7 @@ function link_port_python () {
 function port_python_alias () {
     
 
-    PYV=$(port_python_version)
+    PYVERSION=$(port_python_version)
     
     PORT_BIN="$MPPREFIX/bin"
     PORT_PY_BIN="$MPPREFIX/Library/Frameworks/Python.framework/Versions/$PYVERSION/bin"
@@ -51,7 +51,6 @@ function port_python_alias () {
         export PYTHONPATH="${PYTHONPATH}:~/.python/lib/python/site-packages:~/.python/lib/python$PYVERSION/site-packages"
         export PYTHONPATH="${PYTHONPATH}:~/.python/lib/python:~/.python/lib/python$PYVERSION"
     fi
-    
     # Add python bin/ directories to the shell path.
     export PATH="$HOME/Library/Python/$PYVERSION/bin/:$MPPREFIX/Library/Frameworks/Python.framework/Versions/$PYVERSION/bin/:$PATH"
     STSCI="/usr/local/stsci/$PYDIR/lib/python"
@@ -61,7 +60,8 @@ function port_python_alias () {
     fi
     
     py3exe=`find $MPPREFIX/bin -name python3.[0-9]`
-    if [ -f $py3exe ]; then
+    if [[ -f $py3exe ]]; then
+		
       if [ ! -e "$MPPREFIX/bin/python3" ]; then
         echo "Linking $py3exe to python3"
         sudo ln -s "$py3exe" "$MPPREFIX/bin/python3"
@@ -71,8 +71,9 @@ function port_python_alias () {
 
 if [ -f $MPPREFIX/bin/port ]; then
 	
-	port_use_python=$(port select --show python 2>&1 | grep "Error")
-	if [[ -z $port_use_python ]]; then
+	port_use_python=`port select --show python 2>&1 | grep "Error"`
+	
+	if [[ -z "$port_use_python" ]]; then
 	    	port_python_alias
 	fi
 fi
