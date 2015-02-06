309de84550bfdcfa2e763afc9884f12779862665
Alex Rudy <alex.rudy@gmail.com>
2015-02-05 20:19:11 -0800

    TMux / MacPorts

--- macports/path.bash
+++ macports/path.bash
@@ -2,3 +2,7 @@ if [ -f "$MPPREFIX/bin/port" ]; then
     export PATH="$MPPREFIX/bin:$MPPREFIX/sbin:$PATH"                            #Macports Path Prefix
     export PATH="$PATH:$MPPREFIX/libexec/perl5.12/sitebin"
 fi
+
+tmp () {
+    tmux-start MacPorts
+}
