d5c0b3e25491b111e5513564d66927baa48983a9
Alex Rudy <alex.rudy@gmail.com>
2016-08-12 10:57:30 -0700

    Support homebrew in place of macports for python

--- python/path.sh
+++ python/path.sh
@@ -2,9 +2,11 @@
 
 if [[ -e "/usr/local/bin/virtualenvwrapper.sh" ]]; then
 	VIRTUALENVWRAPPER="/usr/local/bin/virtualenvwrapper.sh"
+	VIRTUALENVWRAPPER_PYTHON="/usr/local/bin/python"
 else
 	if [[ -e "$MPPREFIX/bin/virtualenvwrapper.sh" ]]; then
 		VIRTUALENVWRAPPER="$MPPREFIX/bin/virtualenvwrapper.sh"
+		VIRTUALENVWRAPPER_PYTHON="/opt/local/bin/python"
 	fi
 fi
 
@@ -15,6 +17,6 @@ if [ -f "$VIRTUALENVWRAPPER" ]; then
     if [ -d "$HOME/Development/" ]; then
         export PROJECT_HOME="$HOME/Development/"
     fi
-	VIRTUALENVWRAPPER_PYTHON=$(which python)
+	export VIRTUALENVWRAPPER_PYTHON
     source $VIRTUALENVWRAPPER
 fi
--- virtualenv/env.sh
+++ virtualenv/env.sh
@@ -1,1 +1,5 @@
+
+if [ -f '/usr/local/bin/python' ]; then
+  export VIRTUALENVWRAPPER_PYTHON='/usr/local/bin/python'
+fi
 export VIRTUAL_ENV_DISABLE_PROMPT=1
