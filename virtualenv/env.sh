b5488353a0789e05d3325117f39777766943b48d
Alex Rudy <alex.rudy@gmail.com>
2016-12-16 11:16:01 -0800

    Update to virtualenv environment variables

--- virtualenv/env.sh
+++ virtualenv/env.sh
@@ -1,5 +1,8 @@
-
-if [ -f '/usr/local/bin/python' ]; then
-  export VIRTUALENVWRAPPER_PYTHON='/usr/local/bin/python'
+if [ -d "$MPPREFIX" ]; then
+    export VIRTUALENVWRAPPER_PYTHON="$MPPREFIX/bin/python"
+else
+    if [ -f '/usr/local/bin/python' ]; then
+        export VIRTUALENVWRAPPER_PYTHON='/usr/local/bin/python'
+    fi
 fi
 export VIRTUAL_ENV_DISABLE_PROMPT=1
