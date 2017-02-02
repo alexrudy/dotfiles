3c19869fdfc307a9ab3c5f0167121bc7bbb433dd
Alex Rudy <alex.rudy@gmail.com>
2017-02-02 12:23:36 -0800

    Demoting OSX server paths

---
+++ osx/path.sh
@@ -0,0 +1,4 @@
+if [ -d "/Applications/Server.app/Contents/ServerRoot/usr/" ]; then
+    pathdemote /Applications/Server.app/Contents/ServerRoot/usr/bin
+    pathdemote /Applications/Server.app/Contents/ServerRoot/usr/sbin
+fi
