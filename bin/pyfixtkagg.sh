332fe84eb675b9d99a5cec1c1edd00c366479dbd
Alex Rudy <alex.rudy@gmail.com>
2017-05-31 15:12:25 -0700

    Updates to ZSH configuration

--- bin/pyfixtkagg.sh
+++ bin/pyfixtkagg.sh
@@ -2,12 +2,14 @@
 PYVERSION=`python -c 'import sys; print("{0.major:d}.{0.minor:d}".format(sys.version_info))'`
 
 TARGET_SP="$VIRTUAL_ENV/lib/python$PYVERSION/site-packages"
-SOURCE_SP="$MPPREFIX/Library/Frameworks/Python.framework/Versions/$PYVERSION/lib/python$PYVERSION/site-packages"
+SOURCE_PREFIX=$(cat $VIRTUAL_ENV/lib/python$PYVERSION/orig-prefix.txt)
+SOURCE_SP="$SOURCE_PREFIX/lib/python$PYVERSION/site-packages"
 
 if ! find $TARGET_SP -name "_tkinter.*"; then
     echo "Found _tkinter"
-    echo `find $TARGET_SP -name "_tkinter.*"`
+    echo $(find $TARGET_SP -name "_tkinter.*")
 else
-    SOURCE=`find $SOURCE_SP -name "_tkinter.*"`
+    SOURCE=$(find $SOURCE_SP -name "_tkinter.*")
+    echo "Found source $SOURCE"
     ln -s $SOURCE $TARGET_SP/`basename $SOURCE`
 fi
