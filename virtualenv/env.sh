ec805a67128e2d526faca9dadb80964ebd363d87
Alex Rudy <alexander.rudy@lendup.com>
2017-07-18 14:30:18 -0700

    fix detection of macports

--- virtualenv/env.sh
+++ virtualenv/env.sh
@@ -2,7 +2,7 @@ if [ -f '/usr/local/bin/python3' ]; then
     export VIRTUALENVWRAPPER_PYTHON='/usr/local/bin/python3'
 elif [ -f "/usr/local/bin/python" ]; then
 	export VIRTUALENVWRAPPER_PYTHON='/usr/local/bin/python'
-elif [ -d "$MPPREFIX" ]; then
+elif [ -f "$MPPREFIX/bin/python" ]; then
     export VIRTUALENVWRAPPER_PYTHON="$MPPREFIX/bin/python"
 fi
 export VIRTUAL_ENV_DISABLE_PROMPT=1
