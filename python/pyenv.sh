b1e3b90a632218088454685f6c357b0893fb3676
Alex Rudy <alexander.rudy@lendup.com>
2017-09-18 15:20:19 -0700

    Use pyenv virtualenv

--- python/pyenv.sh
+++ python/pyenv.sh
@@ -1,6 +1,8 @@
 type pyenv &> /dev/null
 if [ $? -eq 0 ]; then
-	if which pyenv-virtualenv-init > /dev/null; then eval "$(pyenv virtualenv-init -)"; fi
 	eval "$(pyenv init -)"
+	if which pyenv-virtualenvwrapper > /dev/null; then
+    eval "$(pyenv virtualenvwrapper init -)"
+  fi
 fi
 
