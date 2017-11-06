235b93bd2ff3ba0a7816e6bd075c426a7069680b
Alex Rudy <alexander.rudy@lendup.com>
2017-11-06 14:55:04 -0800

    Add google cloud SDK to path if it is around.

---
+++ gcsdk/path.zsh
@@ -0,0 +1,6 @@
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f "$HOME/.google/google-cloud-sdk/path.zsh.inc" ]; then source "$HOME/.google/google-cloud-sdk/path.zsh.inc"; fi
+
+# The next line enables shell command completion for gcloud.
+if [ -f "$HOME/.google/google-cloud-sdk/completion.zsh.inc" ]; then source "$HOME/.google/google-cloud-sdk/completion.zsh.inc"; fi
