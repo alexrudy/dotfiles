ae3e86ed99a6d326f5634ede22a799d1a9f4e75e
Alex Rudy <alexander.rudy@lendup.com>
2018-02-15 12:43:12 -0800

    Script to download hubignore

---
+++ bin/hubignore.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import click
+import requests
+
+@click.command()
+@click.argument("language")
+@click.option("--append/--no-append", default=True)
+def main(language, append):
+    """Get a language's git ignore"""
+    resp = requests.get(f"https://github.com/github/gitignore/raw/master/{language}.gitignore")
+    with open(".gitignore", 'a' if append else 'w') as handle:
+        handle.write(resp.text)
+    
+if __name__ == '__main__':
+    main()
