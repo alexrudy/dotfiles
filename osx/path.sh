94bea910b6525fb6b08902423ade1e931ab8e74a
Alex Rudy <alex.rudy@gmail.com>
2018-10-01 11:43:09 -0700

    Add defaultbrowser

--- osx/path.sh
+++ osx/path.sh
@@ -3,3 +3,7 @@ if [ -d "/Applications/Server.app/Contents/ServerRoot/usr/" ]; then
     pathdemote /Applications/Server.app/Contents/ServerRoot/usr/sbin
 fi
 pathadd "$DOTFILES/osx/bin"
+
+if [ -f "$DOTFILES/osx/defaultbrowser/defaultbrowser" ]; then
+  ln -s "$DOTFILES/osx/defaultbrowser/defaultbrowser" "$DOTFILES/osx/bin/defaultbrowser"
+fi
