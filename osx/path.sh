02340b3716e59286075ece9e35a443977bfa1086
Alex Rudy <alex.rudy@gmail.com>
2019-10-28 11:05:15 -0700

    Fixup missing xcode-select

--- osx/path.sh
+++ osx/path.sh
@@ -8,7 +8,7 @@ if [ -f "$DOTFILES/osx/defaultbrowser/defaultbrowser" ]; then
   mv "$DOTFILES/osx/defaultbrowser/defaultbrowser" "$DOTFILES/osx/bin/defaultbrowser"
 fi
 
-if which xcode-select > /dev/null; then
+if which xcode-select 2> /dev/null; then
     XCODE_PATH=$(xcode-select -p)
     if [ -d $XCODE_PATH ]; then
       pathprepend $XCODE_PATH/usr/bin
