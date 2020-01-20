1956484fa205712a6834e1e3a9d8568bbd48e4b0
Alex Rudy <alex.rudy@gmail.com>
2020-01-20 11:37:00 -0800

    Installers for rbenv and pyenv

---
+++ python/install-pyenv.sh
@@ -0,0 +1,12 @@
+if ! command_exists pyenv; then
+    git clone https://github.com/pyenv/pyenv.git $HOME/.pyenv
+fi
+
+if command_exists pyenv; then
+    if [[ ! -d "$(pyenv root)/plugins/xxenv-latest" ]]; then
+        git clone https://github.com/momo-lab/xxenv-latest.git "$(pyenv root)"/plugins/xxenv-latest
+    fi
+    if ! command_exists pyenv-virtualenv; then
+        git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
+    fi
+fi
---
+++ ruby/install-rbenv.sh
@@ -0,0 +1,6 @@
+
+if command_exists rbenv; then
+    if [[ ! -d "$(rbenv root)/plugins/xxenv-latest" ]]; then
+        git clone https://github.com/momo-lab/xxenv-latest.git "$(rbenv root)"/plugins/xxenv-latest
+    fi
+fi
