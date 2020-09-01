dd1e0c8d56933fe408dd169d135f18c91df11a99
Alex Rudy <alex.rudy@gmail.com>
2020-09-01 09:43:54 -0700

    Fix pyenv installation errors

--- python/bin/install-pyenv.sh
+++ python/bin/install-pyenv.sh
@@ -6,7 +6,7 @@ if command_exists pyenv; then
     if [[ ! -d "$(pyenv root)/plugins/xxenv-latest" ]]; then
         git clone https://github.com/momo-lab/xxenv-latest.git "$(pyenv root)"/plugins/xxenv-latest
     fi
-    if ! command_exists pyenv-virtualenv; then
+    if [[ ! -d "$(pyenv root)/plugins/pyenv-virtualenv" ]]; then
         git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
     fi
 fi
