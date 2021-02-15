802718bb59c34b5768db00bb98799e1e339dd75f
Alex Rudy <alex.rudy@gmail.com>
2021-02-15 11:35:22 -0800

    Fixup some pyenv things

--- python/path.sh
+++ python/path.sh
@@ -1,2 +1,12 @@
+if [[ -d "$HOME/.pyenv" ]]; then
+    export PYENV_ROOT="$HOME/.pyenv"
+    pathadd "$PYENV_ROOT/bin"
+
+    eval "$(pyenv init -)"
+    eval "$(pyenv virtualenv-init -)"
+fi
+
 pathadd $DOTFILES/python/bin
 pathadd $HOME/.local/bin
+
+pathpromote $HOME/.local/bin
--- python/pyenv.sh
+++
@@ -1,4 +0,0 @@
-if [[ -d "$HOME/.pyenv" ]]; then
-    export PYENV_ROOT="$HOME/.pyenv"
-    pathadd "$PYENV_ROOT/bin"
-fi
