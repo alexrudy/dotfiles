5e055eed6f240e83575751961387da33d5238ccf
Alex Rudy <opensource@alexrudy.net>
2021-06-21 10:24:15 -0700

    Fix pyenv init arguments

--- python/path.sh
+++ python/path.sh
@@ -2,7 +2,7 @@ if [[ -d "$HOME/.pyenv" ]]; then
     export PYENV_ROOT="$HOME/.pyenv"
     pathadd "$PYENV_ROOT/bin"
 
-    eval "$(pyenv init -)"
+    eval "$(pyenv init --path)"
     if command_exists pyenv-virtualenv-init; then
         eval "$(pyenv virtualenv-init -)"
     fi
