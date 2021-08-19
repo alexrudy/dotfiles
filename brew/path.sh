8d25f925aad7976f8868ce26a60156f64f0fefa2
Alex Rudy <opensource@alexrudy.net>
2021-08-19 10:32:56 -0700

    Support M1/arm homebrew

--- brew/path.sh
+++ brew/path.sh
@@ -3,3 +3,8 @@ if [ -f "/usr/local/bin/brew" ]; then
 	pathprepend "/usr/local/opt/llvm/bin"
 	export LDFLAGS="-L/usr/local/opt/llvm/lib"
 fi
+
+if [ -f "/opt/homebrew/bin/brew" ]; then
+	eval "$(/opt/homebrew/bin/brew shellenv)"
+		
+fi
