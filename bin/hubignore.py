731bfc853da1a67093fbbfc16ed712f0e554c01b
Alex Rudy <opensource@alexrudy.net>
2021-09-02 12:47:37 -0700

    Improve hubignore

--- bin/hubignore.py
+++ bin/hubignore.py
@@ -3,12 +3,29 @@
 import click
 import requests
 
+BASE_URL = "https://raw.githubusercontent.com/github/gitignore/master/"
+
 @click.command()
 @click.argument("language")
 @click.option("--append/--no-append", default=True)
 def main(language, append):
     """Get a language's git ignore"""
-    resp = requests.get(f"https://raw.githubusercontent.com/github/gitignore/raw/master/{language}.gitignore")
+
+    variations = [language.capitalize(), language.lower(), language.upper()]
+
+
+
+    initial_resp = requests.get(f"{BASE_URL}{language}.gitignore")
+    if initial_resp.status_code == 200:
+        resp = initial_resp
+    else:
+        for variation in variations:
+            resp = requests.get(f"{BASE_URL}{variation}.gitignore")
+            if resp.status_code == 200:
+                break
+        else:
+            initial_resp.raise_for_status()
+
     with open(".gitignore", 'a' if append else 'w') as handle:
         handle.write(resp.text)
     
