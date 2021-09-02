ae99c92e25b4618f0d916c7951b31c32734559b9
Alex Rudy <opensource@alexrudy.net>
2021-09-02 12:50:15 -0700

    Fixups for hubignore and gitconfig

--- bin/hubignore.py
+++ bin/hubignore.py
@@ -5,6 +5,7 @@ import requests
 
 BASE_URL = "https://raw.githubusercontent.com/github/gitignore/master/"
 
+
 @click.command()
 @click.argument("language")
 @click.option("--append/--no-append", default=True)
@@ -12,8 +13,6 @@ def main(language, append):
     """Get a language's git ignore"""
 
     variations = [language.capitalize(), language.lower(), language.upper()]
-
-
 
     initial_resp = requests.get(f"{BASE_URL}{language}.gitignore")
     if initial_resp.status_code == 200:
@@ -26,8 +25,9 @@ def main(language, append):
         else:
             initial_resp.raise_for_status()
 
-    with open(".gitignore", 'a' if append else 'w') as handle:
+    with open(".gitignore", "a" if append else "w") as handle:
         handle.write(resp.text)
-    
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     main()
