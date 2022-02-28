e7769d4071f40a99bbf96c572f87b3fba8f41d60
Alex Rudy <opensource@alexrudy.net>
2022-02-28 07:26:58 -0800

    Tweak homebrew paths for ARM support

--- brew/completion.sh
+++ brew/completion.sh
@@ -1,1 +1,7 @@
+if [ -f "/usr/local/share/zsh-completions" ]; then
 fpath=(/usr/local/share/zsh-completions $fpath)
+fi
+
+if [ -f "/opt/homebrew/share/zsh-completions" ]; then
+fpath=(/opt/homebrew/share/zsh-completions $fpath)
+fi
--- brew/path.sh
+++ brew/path.sh
@@ -6,5 +6,4 @@ fi
 
 if [ -f "/opt/homebrew/bin/brew" ]; then
 	eval "$(/opt/homebrew/bin/brew shellenv)"
-		
 fi
