ff29c7728e8fead194ef828cdfa21f0efafab6fe
Alex Rudy <opensource@alexrudy.net>
2022-07-11 09:49:39 -0700

    Guard config for brew

--- autojump/config.zsh
+++ autojump/config.zsh
@@ -1,1 +1,3 @@
-[[ -f $(brew --prefix)/etc/profile.d/autojump.sh ]] && . $(brew --prefix)/etc/profile.d/autojump.sh
+if command_exists brew; then
+    [[ -f $(brew --prefix)/etc/profile.d/autojump.sh ]] && . $(brew --prefix)/etc/profile.d/autojump.sh
+fi
