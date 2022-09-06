fedf5625a035bb1752a5371350c82077e60287c2
Alex Rudy <alex.rudy@discordapp.com>
2022-09-06 21:37:37 +0000

    Fix autojump init when installed directly

--- autojump/config.zsh
+++ autojump/config.zsh
@@ -1,3 +1,5 @@
 if command_exists brew; then
     [[ -f $HOMEBREW_PREFIX/etc/profile.d/autojump.sh ]] && . $HOMEBREW_PREFIX/etc/profile.d/autojump.sh
 fi
+
+[[ -s $HOME/.autojump/etc/profile.d/autojump.sh ]] && . $HOME/.autojump/etc/profile.d/autojump.sh
