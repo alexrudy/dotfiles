3d4a1a6b6a43b7d086dfab545a2f43740ac71eff
Alex Rudy <alex.rudy@discordapp.com>
2022-09-29 10:17:48 -0700

    Fix up additional install scripts

--- python/bin/install-pyenv.sh
+++ python/bin/install-pyenv.sh
@@ -1,14 +1,16 @@
+#!/usr/bin/env sh
+
 if ! [ -e "${HOME}/.pyenv" ]; then
     git clone https://github.com/pyenv/pyenv.git "${HOME}/.pyenv"
 fi
 
-. ${DOTFILES}/python/pyenv.sh
+. "${DOTFILES}/python/pyenv.sh"
 
 if command_exists pyenv; then
-    if [[ ! -d "$(pyenv root)/plugins/xxenv-latest" ]]; then
+    if [ ! -d "$(pyenv root)/plugins/xxenv-latest" ]; then
         git clone https://github.com/momo-lab/xxenv-latest.git "$(pyenv root)/plugins/xxenv-latest"
     fi
-    if [[ ! -d "$(pyenv root)/plugins/pyenv-virtualenv" ]]; then
+    if [ ! -d "$(pyenv root)/plugins/pyenv-virtualenv" ]; then
         git clone https://github.com/pyenv/pyenv-virtualenv.git "$(pyenv root)/plugins/pyenv-virtualenv"
     fi
 fi
--- ruby/bin/install-rbenv.sh
+++ ruby/bin/install-rbenv.sh
@@ -1,6 +1,7 @@
+#!/usr/bin/env sh
 
 if command_exists rbenv; then
-    if [[ ! -d "$(rbenv root)/plugins/xxenv-latest" ]]; then
-        git clone https://github.com/momo-lab/xxenv-latest.git "$(rbenv root)"/plugins/xxenv-latest
+    if [ ! -d "$(rbenv root)/plugins/xxenv-latest" ]; then
+        git clone https://github.com/momo-lab/xxenv-latest.git "$(rbenv root)/plugins/xxenv-latest"
     fi
 fi
