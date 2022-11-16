29388cfb5f79dfb082863c45f660cd7472fc18bd
Alex Rudy <github@alexrudy.net>
2022-11-16 09:09:57 -0800

    Enable fzf when installed from apt

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -1,1 +1,5 @@
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
+
+[ -f /usr/share/doc/fzf/examples/completion.zsh ] && source /usr/share/doc/fzf/examples/completion.zsh
+[ -f /usr/share/doc/fzf/examples/key-bindings.zsh ] && source /usr/share/doc/fzf/examples/key-bindings.zsh
