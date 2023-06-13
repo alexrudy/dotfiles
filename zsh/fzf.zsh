b5203ec66d446a352cc46c756abdbd5218be67e8
Alex Rudy <github@alexrudy.net>
2023-06-13 04:18:19 +0000

    Fix command check in fzf

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -1,6 +1,9 @@
+command_exists () {
+    type "$1" &> /dev/null ;
+}
 
 # Only run fzf if we can't find atuin.
-if !command_exists atuin; then
+if ! command_exists atuin; then
     [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
     [ -f /usr/share/doc/fzf/examples/completion.zsh ] && source /usr/share/doc/fzf/examples/completion.zsh
     [ -f /usr/share/doc/fzf/examples/key-bindings.zsh ] && source /usr/share/doc/fzf/examples/key-bindings.zsh
