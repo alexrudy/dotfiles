03c9d7c676a2fc43c91ea9c3119616e032dac9af
Alex Rudy <alex.rudy@discordapp.com>
2023-07-07 22:52:44 +0000

    Fix fzf installer

--- installers/install-fzf.sh
+++ installers/install-fzf.sh
@@ -5,8 +5,10 @@ set -eu
 . "${DOTFILES}/installers/prelude.sh"
 
 if ! command_exists fzf && ! test -f ~/.fzf.zsh && ! command_exists brew; then
-    _process "🚛 install fzf"
-    git clone --depth 1 https://github.com/junegunn/fzf.git "${HOME}/.fzf"
-    "${HOME}/.fzf/install" --bin --no-update-rc
-    _finished "✅ fzf installed"
+    if ! test -d ~/.fzf; then
+        _process "🚛 install fzf"
+        git clone --depth 1 https://github.com/junegunn/fzf.git "${HOME}/.fzf"
+        "${HOME}/.fzf/install" --bin --no-update-rc
+        _finished "✅ fzf installed"
+    fi
 fi
