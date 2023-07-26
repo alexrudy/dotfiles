c0517b04cd4e47038dd6f3245572d95c90245295
Alex Rudy <github@alexrudy.net>
2023-07-26 19:28:22 +0000

    Fix syntax for autojump configuration

--- autojump/config.zsh
+++ autojump/config.zsh
@@ -1,7 +1,7 @@
 if command_exists brew; then
     [[ -f $HOMEBREW_PREFIX/etc/profile.d/autojump.sh ]] && . $HOMEBREW_PREFIX/etc/profile.d/autojump.sh
-elif [[ -s $HOME/.autojump/etc/profile.d/autojump.sh ]]
+elif [[ -s $HOME/.autojump/etc/profile.d/autojump.sh ]]; then
     . $HOME/.autojump/etc/profile.d/autojump.sh
-elif [[ -s /etc/profile.d/autojump.sh ]]
+elif [[ -s /etc/profile.d/autojump.sh ]]; then
     . /etc/profile.d/autojump.sh
 fi
