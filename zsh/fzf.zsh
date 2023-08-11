e5c898a923dfe057ee0e777882e1a111a1a6e433
Alex Rudy <alex.rudy@discordapp.com>
2023-08-11 18:34:09 +0000

    Tweak fzf setup

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -2,9 +2,13 @@ command_exists () {
     type "$1" &> /dev/null ;
 }
 
+# Add fzf to path
+if [ -f "${HOME}/.fzf/bin" ]; then
+    pathadd "${HOME}/.fzf/bin"
+fi
+
 # Only run fzf if we can't find atuin.
 if ! command_exists atuin; then
-    [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
     [ -f /usr/share/doc/fzf/examples/completion.zsh ] && source /usr/share/doc/fzf/examples/completion.zsh
     [ -f /usr/share/doc/fzf/examples/key-bindings.zsh ] && source /usr/share/doc/fzf/examples/key-bindings.zsh
 fi
