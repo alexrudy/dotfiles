724b2cee84924b987052a190a69bd681ceec884a
Alex Rudy <github@alexrudy.net>
2023-10-31 20:20:54 +0000

    Clean up some git configs

--- onepassword/config.zsh
+++ onepassword/config.zsh
@@ -1,2 +1,11 @@
 fpath=($DOTFILES/onepassword/functions $fpath)
 autoload -U $DOTFILES/onepassword/functions/*(:t)
+
+OP_CONTAINER="${HOME}/Library/Group Containers/2BUA8C4S2C.com.1password/"
+
+if test -S "${OP_CONTAINER}/t/agent.sock"; then
+    if test ! -e "${HOME}/.1password/agent.sock"; then
+        ln -s "${OP_CONTAINER}/t/agent.sock" "${HOME}/.1password/agent.sock"
+    fi
+    export SSH_AUTH_SOCK="${HOME}/.1password/agent.sock"
+fi
