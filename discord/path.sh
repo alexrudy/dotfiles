f97664986b2af14497963aac51a82e72b3b53489
Alex Rudy <alex.rudy@discordapp.com>
2024-01-10 19:49:34 +0000

    Discord paths

---
+++ discord/path.sh
@@ -0,0 +1,10 @@
+if [ -d "$HOME/Development/discord/discord/.git" ]; then
+    pathprepend "$HOME/Development/discord/discord/.local/bin"
+    pathprepend "$DOTFILES/discord/bin"
+fi
+
+if [ -e "$HOME/Development/discord/discord/clyde" ]; then
+    if [ ! -f "$DOTFILES/discord/bin/clyde" ]; then
+        ln -s "$HOME/Development/discord/discord/clyde" "$DOTFILES/discord/bin/clyde"
+    fi
+fi
