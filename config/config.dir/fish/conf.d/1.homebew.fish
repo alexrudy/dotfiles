4c020ace9ef6007d9381f6be3a1bb9a0483fbd69
Alex Rudy <github@alexrudy.net>
2025-02-22 10:46:26 -0800

    Fix brew fish config

--- config/config.dir/fish/conf.d/1.homebew.fish
+++ config/config.dir/fish/conf.d/1.homebew.fish
@@ -1,4 +1,4 @@
-if !command -v brew > /dev/null
+if not command -v brew > /dev/null
     if test -d /opt/homebrew/bin/brew
         fish_add_path -p /opt/homebrew/bin
         fish_add_path -a /opt/homebrew/sbin
