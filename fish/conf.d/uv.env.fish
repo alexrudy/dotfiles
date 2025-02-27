07a5638ae267bc071e2324cb488bf39b3febd7f3
Alex Rudy <github@alexrudy.net>
2025-02-27 13:06:23 -0800

    fish: guard against missing env file

--- fish/conf.d/uv.env.fish
+++ fish/conf.d/uv.env.fish
@@ -1,4 +1,7 @@
 if test -d "$HOME/.local/bin"
     fish_add_path "$HOME/.local/bin"
-    source "$HOME/.local/bin/env.fish"
+
+    if test -f "$HOME/.local/bin/env.fish"
+        source "$HOME/.local/bin/env.fish"
+    end
 end
