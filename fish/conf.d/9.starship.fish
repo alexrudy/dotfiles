975b4bf54d73ca97b716e87cfd465f5d36368d15
Alex Rudy <github@alexrudy.net>
2025-12-15 17:31:53 +0000

    Improved fish config setup for Discord

--- fish/conf.d/9.starship.fish
+++ fish/conf.d/9.starship.fish
@@ -1,3 +1,7 @@
 if status is-interactive; and command -v starship > /dev/null
     starship init fish | source
+    if test -e "/opt/coder/coder"
+        test -z "$XDG_CONFIG_PATH"; and set --local XDG_CONFIG_PATH "$HOME/.config"
+        set -gx STARSHIP_CONFIG "$XDG_CONFIG_PATH/starship.discord.toml"
+    end
 end
