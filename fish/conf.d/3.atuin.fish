602708724c91e4e8d76ed7db2bcb214eedd55d12
Alex Rudy <github@alexrudy.net>
2025-12-21 08:52:37 -0800

    Fix atuin config for fish

--- fish/conf.d/3.atuin.fish
+++ fish/conf.d/3.atuin.fish
@@ -1,10 +1,6 @@
 
 
 if status is-interactive
-    if test -f "$HOME/.atuin/bin/env.fish"
-        source "$HOME/.atuin/bin/env.fish"
-    end
-
     if command -v atuin > /dev/null
         atuin init fish | source
     end
